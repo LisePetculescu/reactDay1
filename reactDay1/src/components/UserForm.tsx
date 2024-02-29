056f4e2c0218007097d19ddb4d3bee415a8f33f2
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-02-29 22:11:49 +0100

    userForm done

--- reactDay1/src/components/UserForm.tsx
+++ reactDay1/src/components/UserForm.tsx
@@ -1,5 +1,5 @@
 import { BaseProps } from "../types";
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 import { User } from "../data/data";
 
 type UserFormProps = BaseProps & {
@@ -7,19 +7,30 @@ type UserFormProps = BaseProps & {
 };
 
 export default function UserForm({ title, onSubmitUser }: UserFormProps) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const isActiveRef = useRef<HTMLInputElement>(null);
+
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const newUser: User = {
+      name: nameRef.current?.value || "",
+      email: emailRef.current?.value || "",
+      isActive: isActiveRef.current?.checked || false,
+    };
+
     //Read form inputs and submit the form to the parent
-    //onSubmitUser(newUser);
+    onSubmitUser(newUser);
   };
 
   return (
     <>
       <h2>{title}</h2>
       <form onSubmit={onSubmit}>
-        First Name: <input name="name" />
-        Email: <input name="email" />
-        Active: <input type="checkbox" name="isActive" />
+        First Name: <input ref={nameRef} name="name" />
+        Email: <input ref={emailRef} name="email" />
+        Active: <input ref={isActiveRef} type="checkbox" name="isActive" />
         <button type="submit">Add User</button>
       </form>
     </>
